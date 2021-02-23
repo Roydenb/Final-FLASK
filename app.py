76709ffa1d6ebf8ee5cc18d514832c97fec9f158
Royden <jaydendane2@gmail.com>
2021-02-23 16:05:48 +0200

    Updated the functions, working on creating more than one table

--- app.py
+++ app.py
@@ -1,44 +1,95 @@
-import sqlite3
 from flask import Flask, render_template, request
+import sqlite3 as sql
 
 
 app = Flask(__name__)
 
-def init_sqlite_db():
-    conn = sqlite3.connect('database.db')
-    print("Opened database successfully")
-    conn.execute('CREATE TABLE students (name TEXT, surname TEXT, email TEXT, contact TEXT)')
-    print("Table created successfully")
-    conn.close()
-
-    init_sqlite_db()
 
 @app.route('/')
-@app.route('/index/')
-def enter_new_student():
+def newuser():
     return render_template('index.html')
 
-@app.route('/add-new-user/', methods=['POST'])
-def add_new_record():
-    if request.method == "POST":
+@app.route('/')
+def newadmin():
+    return render_template('admin.html')
 
+# FOR USER
+@app.route('/adduser/', methods=['POST', 'GET'])
+def adduser():
+    if request.method == 'POST':
         try:
-            name = request.form['name']
-            sur = request.form['surname']
+            nm = request.form['name']
+            surname = request.form['surname']
             mail = request.form['email']
-            cont = request.form['contact']
+            num = request.form['contact']
 
-            with sqlite3.connect('database.db') as con:
+            with sql.connect("database.db") as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO Users (name, surname, email, contact) VALUES (?, ?, ?, ?)", (name, sur, mail, cont))
+                con.execute('CREATE TABLE IF NOT EXISTS Users (name TEXT, surname TEXT, email TEXT, contact TEXT)')
+                cur.execute("INSERT INTO Users (name,surname,email,contact) VALUES (?,?,?,?)", (nm, surname, mail, num))
                 con.commit()
-                msg = "Record successfully added."
+                msg = "Record successfully added"
 
-        except Exception as e:
+
+        except:
             con.rollback()
-            msg = "Error occurred in insert operation: " + e
+            msg = "error in insert operation"
+
         finally:
+            return render_template("success.html", msg=msg)
             con.close()
-            return render_template('success.html', msg=msg)
+
+@app.route('/list')
+def list():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("select * from Users")
+
+    rows = cur.fetchall()
+    return render_template("records.html", rows=rows)
+
+# FOR ADMIN
+# FOR A ADMIN TO VIEW THE LIST
+# NEED TO CREATE A BUTTON THAT GOES TO THE ADMIN
+# ADMIN NEED TO HAVE A LOGIN BEFORE THEY CAN VIEW THE INFORMATION
+# @app.route('/adadmin/', methods=['POST', 'GET'])
+# def adduser():
+#     if request.method == 'POST':
+#         try:
+#             a_nm = request.form['admin-name']
+#             a_surname = request.form['admin-surname']
+#             a_mail = request.form['admin-email']
+#             a_passw = request.form['admin-password']
+#
+#             with sql.connect("database.db") as con:
+#                 cur = con.cursor()
+#                 con.execute('CREATE TABLE IF NOT EXISTS Admin (name TEXT, surname TEXT, email TEXT, contact TEXT)')
+#                 cur.execute("INSERT INTO Admin (admin-name,admin-surname,admin-email,admin-password) VALUES (?,?,?,?)", (a_nm, a_surname, a_mail, a_passw))
+#                 con.commit()
+#                 msg = "Record successfully added"
+#
+#
+#         except:
+#             con.rollback()
+#             msg = "error in insert operation"
+#
+#         finally:
+#             return render_template("success.html", msg=msg)
+#             con.close()
+#
+# @app.route('/list')
+# def list():
+#     con = sql.connect("database.db")
+#     con.row_factory = sql.Row
+#
+#     cur = con.cursor()
+#     cur.execute("select * from Users")
+#
+#     rows = cur.fetchall()
+#     return render_template("records.html", rows=rows)
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
